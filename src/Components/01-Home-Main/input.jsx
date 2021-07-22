aaa6d63037eced0f55695a99970665d0f70d89a5
akpinkerton <akpinkerton@gmail.com>
2021-07-21 18:17:20 -0700

    buttons r missing

--- src/Components/01-Home-Main/input.jsx
+++ src/Components/01-Home-Main/input.jsx
@@ -5,7 +5,63 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
     return (
       <>
-THIS IS where you can input a resources
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h3>
+				h3. Lorem ipsum dolor sit amet.
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<ul class="nav">
+				<li class="nav-item">
+					<a class="nav-link active" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Profile</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#">Messages</a>
+				</li>
+				<li class="nav-item dropdown ml-md-auto">
+					 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+						 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+						<div class="dropdown-divider">
+						</div> <a class="dropdown-item" href="#">Separated link</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8">
+		</div>
+		<div class="col-md-4">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="btn-group" role="group">
+
+				<button class="btn btn-secondary" type="button">
+					Left
+				</button>
+				<button class="btn btn-secondary" type="button">
+					Center
+				</button>
+				<button class="btn btn-secondary" type="button">
+					Right
+				</button>
+				<button class="btn btn-secondary" type="button">
+					Justify
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
       </>
     )
   }
